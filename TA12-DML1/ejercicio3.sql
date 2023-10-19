7aaabe2b2dc6a42226282b41ef56165bc22ca61b
sergial273 <97388630+sergial273@users.noreply.github.com>
2023-10-19 19:02:50 +0200

    Update ejercicio3.sql

--- TA12-DML1/ejercicio3.sql
+++ TA12-DML1/ejercicio3.sql
@@ -9,6 +9,20 @@ create table Comunidad(
     nombre varchar(20),
     primary key (id_comunidad)
 );
+
+insert into Comunidad (nombre) values ('China');
+insert into Comunidad (nombre) values ('Brazil');
+insert into Comunidad (nombre) values ('Indonesia');
+insert into Comunidad (nombre) values ('Portugal');
+insert into Comunidad (nombre) values ('China');
+insert into Comunidad (nombre) values ('Ireland');
+insert into Comunidad (nombre) values ('Egypt');
+insert into Comunidad (nombre) values ('China');
+insert into Comunidad (nombre) values ('Indonesia');
+insert into Comunidad (nombre) values ('Uganda');
+
+select * from Comunidad;
+
 create table Provincia(
 	id_provincia int auto_increment,
     nombre varchar(20),
@@ -17,6 +31,20 @@ create table Provincia(
     primary key (id_provincia),
     foreign key (id_comunidad) references Comunidad (id_comunidad) on delete cascade on update cascade
 );
+
+insert into Provincia (nombre, superficie, id_comunidad) values ('Brazil', 924, 7);
+insert into Provincia (nombre, superficie, id_comunidad) values ('Russia', 234, 6);
+insert into Provincia (nombre, superficie, id_comunidad) values ('Mongolia', 161, 1);
+insert into Provincia (nombre, superficie, id_comunidad) values ('Indonesia', 142, 9);
+insert into Provincia (nombre, superficie, id_comunidad) values ('Philippines', 450, 4);
+insert into Provincia (nombre, superficie, id_comunidad) values ('China', 846, 8);
+insert into Provincia (nombre, superficie, id_comunidad) values ('Philippines', 413, 9);
+insert into Provincia (nombre, superficie, id_comunidad) values ('Portugal', 906, 3);
+insert into Provincia (nombre, superficie, id_comunidad) values ('Afghanistan', 461, 8);
+insert into Provincia (nombre, superficie, id_comunidad) values ('Argentina', 265, 1);
+
+select * from Provincia;
+
 create table Localidad(
 	id_localidad int auto_increment,
     nombre varchar(20),
@@ -27,3 +55,16 @@ create table Localidad(
     primary key (id_localidad),
     foreign key (id_provincia) references Provincia (id_provincia) on delete cascade on update cascade
 );
+
+insert into Localidad (nombre, poblacion, id_provincia, capital_prov, capital_com) values ('Lisieux', 49776, 9, true, false);
+insert into Localidad (nombre, poblacion, id_provincia, capital_prov, capital_com) values ('Gongli', 46233, 3, false, true);
+insert into Localidad (nombre, poblacion, id_provincia, capital_prov, capital_com) values ('Sengeti', 19240, 9, false, true);
+insert into Localidad (nombre, poblacion, id_provincia, capital_prov, capital_com) values ('Horta', 93089, 6, true, true);
+insert into Localidad (nombre, poblacion, id_provincia, capital_prov, capital_com) values ('Onsala', 55525, 5, false, true);
+insert into Localidad (nombre, poblacion, id_provincia, capital_prov, capital_com) values ('Polyarnyy', 74634, 9, false, true);
+insert into Localidad (nombre, poblacion, id_provincia, capital_prov, capital_com) values ('Barreira', 83923, 2, false, true);
+insert into Localidad (nombre, poblacion, id_provincia, capital_prov, capital_com) values ('Ribeirão Preto', 66409, 10, false, true);
+insert into Localidad (nombre, poblacion, id_provincia, capital_prov, capital_com) values ('San Miguel', 50809, 7, true, false);
+insert into Localidad (nombre, poblacion, id_provincia, capital_prov, capital_com) values ('Landskrona', 29811, 10, false, true);
+
+select * from Localidad;
