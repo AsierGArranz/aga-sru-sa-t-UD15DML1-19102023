a4ee906b6e783a582eead0f836ebf8040d145417
sergial273 <97388630+sergial273@users.noreply.github.com>
2023-10-19 17:41:44 +0200

    Update ejercicio4.sql

--- TA15-DML1/ejercicio4.sql
+++ TA15-DML1/ejercicio4.sql
@@ -6,20 +6,65 @@ USE ejercicio10;
 
 create table Cajeros(
 	codigo int auto_increment,
-	nom_apels nvarchar(255),
+	nom_apels varchar(255),
 	primary key (codigo)
 );
+
+insert into Cajeros(nom_apels) values
+('nombre1'),
+('nombre2'),
+('nombre3'),
+('nombre4'),
+('nombre5'),
+('nombre6'),
+('nombre7'),
+('nombre8'),
+('nombre9'),
+('nombre10');
+
+select * from Cajeros;
+
 create table Productos(
 	codigo int auto_increment,
-	nombre nvarchar(100),
-	precio int,
+	nombre varchar(100),
+	precio double,
 	primary key (codigo)
 );
+
+insert into Productos(nombre, precio) values
+('nombre1', 10.3),
+('nombre2', 0.5),
+('nombre3', 80),
+('nombre4', 12.3),
+('nombre5', 10.5),
+('nombre6', 17),
+('nombre7', 99.9),
+('nombre8', 103),
+('nombre9', 100),
+('nombre10', 1);
+
+select * from Productos;
+
 create table Maquinas_registradoras(
 	codigo int auto_increment,
 	piso int,
 	primary key (codigo)
 );
+
+insert into Maquinas_registradoras(piso) values
+(10),
+(0),
+(8),
+(12),
+(10),
+(17),
+(99),
+(1),
+(150),
+(16);
+
+select * from Maquinas_registradoras;
+
 create table Asignado_a(
 	codigo_cajero int,
 	codigo_maquina int,
@@ -29,3 +74,16 @@ create table Asignado_a(
 	foreign key (codigo_maquina) references Maquinas_registradoras (codigo) on delete cascade on update cascade,
 	foreign key (codigo_producto) references Productos (codigo) on delete cascade on update cascade
 );
+insert into Asignado_a values
+(1,1,1),
+(2,2,2),
+(3,3,3),
+(4,4,4),
+(5,5,5),
+(6,6,6),
+(7,7,7),
+(8,8,8),
+(9,9,9),
+(10,10,10);
+
+select * from Asignado_a;
